93689a07d9ae928b32789f33de00c30868acc47d
GinkoDante <ginkodante@gmail.com>
2018-08-02 23:15:22 -0400

    8218: Added additional styling to elements
    Added coloring and styles to graph test

--- app.py
+++ app.py
@@ -4,13 +4,38 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-# Frontend definition:
-app.layout = html.Div(children=[
+# Define a css styling:
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-	html.H1(children='Hello Dash'),
-	html.Div(children='''
-		Dash: A web application framework for Python.
-		'''),
+# Can further customize text by definiing inline styles:
+colors = {
+	# Royal Blue:
+	'background': '#4169E1',
+	# White
+	'text': '#FFFFFF',
+	#White
+	'main_background': '#FFFFFF',
+	# Black
+	'main_text': '#000000'
+}
+
+# Frontend definition:
+app.layout = html.Div(style={'backgroundColor': colors['background'], 'color': colors['text']}, 
+	children=[
+	# children: can be omitted for cleaner code as is always first arg
+	# can be str, int, list, etc...
+	html.H1(children='Hello Dash',
+		style={
+			'textAlign': 'center',
+			'color': colors['text']
+		}
+	),
+	html.Div(children='Dash: A web application framework for Python', 
+		style={
+			'textAlign': 'center',
+			'color': colors['text']
+		}
+	),
 	dcc.Graph(
 		id = 'example-graph',
 		figure={
@@ -20,7 +45,12 @@ app.layout = html.Div(children=[
 				{'x': [1,2,3], 'y': [2,4,5], 'type': 'bar', 'name': u'Montreal'},
 			],
 			'layout': {
-				'title': 'Dash Data Visualization'
+				# 'title': 'Dash Data Visualization',
+				'plot_bgcolor': colors['main_background'],
+				'paper_bgcolor': colors['main_background'],
+				'font': {
+						'color': colors['main_text']
+				}
 			}
 		}
 	)
